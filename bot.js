98cc6314642819e415b4e0a1fd9b2e5ce73aec7e
iVaniet21938129831xxx <42171095+iVaniet21938129831xxx@users.noreply.github.com>
2018-10-04 16:13:58 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1079,9 +1079,9 @@ client.on("message", message => {
 let args = message.content.split(" ").slice(1);
 var argresult = args.join(' '); 
 message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
-m.send(`${argresult}\n **=============================**\n ${m}`);
+m.send(`${argresult}\n ${m}`);
 })
-message.channel.send(`${message.guild.members.filter(m => m.presence.status !== 'all').size} : عدد الاعضاء المستلمين`); 
+message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'all').size}\` : عدد الاعضاء المستلمين`); 
 message.delete(); 
 };     
 });
